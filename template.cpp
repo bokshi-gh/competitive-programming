f361a7638da5113fd2f719783e2e4149279b6862
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-12-10 20:01:49 +0545

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using mii = pair<int,int>;
+using mci = pair<char, int>;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << "\n";
+#else
+#define debug(x)
+#endif
+
+void solve() {
+  // solution
+}
+
+int main() {
+    fastio;
+
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
