6298f248e9414f6287a33a08fcbe25c9e14fd26c
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-12-10 20:34:17 +0545

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -8,22 +8,28 @@ using mci = map<char, int>;
 
 #define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
 
+// ---------------- Debug Macro ----------------
+// Prints variable name and value for debugging
+// Only active if compiled with -DLOCAL
+// Example: g++ -DLOCAL template.cpp -o run
 #ifdef LOCAL
 #define debug(x) cerr << #x << " = " << x << "\n";
 #else
 #define debug(x)
 #endif
 
+
 void solve() {
-  // Solution
+    // Write your solution here
+    // Use debug(x) to print variable values when testing locally
 }
 
 int main() {
     fastio;
 
-    int t = 1;
-    cin >> t;
-    while (t--) solve();
+    int t = 1;           
+    cin >> t;            
+    while (t--) solve(); 
 
     return 0;
 }
